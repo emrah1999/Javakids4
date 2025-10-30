c5496ad6d8afc58938df245b6da6e2158aa7b843
emrah1999 <emraheyubovemrah@gmail.com>
2025-10-30 23:00:34 +0400

    commit Dərs 3. Dəyişənlər

---
+++ java/src/lesson3Variable/Main.java
@@ -0,0 +1,43 @@
+package lesson3Variable;
+
+public class Main {
+    public static void main(String[] args) {
+        byte a=5;
+        byte b=9;
+        byte c=127;
+
+        short n=3;
+        int m=1_000_000_000;
+        System.out.println(m);
+
+        long l=5L;
+
+        float f=5.5f;
+
+        double d=5.5d;
+
+//        System.out.println(0.1+0.1);
+
+        float v=0.1f;
+        float x=0.1f;
+        System.out.println(v+x);
+
+        boolean bool=true;
+        boolean bool1=false;
+
+        char ch='a';
+
+        int sd=3;
+
+        int k;
+        int K;
+
+        String cc="S";
+        System.out.println("sds");
+        String result=sd+cc;
+        System.out.println(result.length());
+
+        System.out.println((int) ch);
+
+    }
+}
