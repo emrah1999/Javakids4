8b71ce2e26f0730f84567739c7557c4eddfd5059
emrah1999 <emraheyubovemrah@gmail.com>
2026-05-25 22:16:09 +0400

    Commit Dərs 60. Spring Logging və Tətbiqin İşə Düşməsi

--- spring/spring-project/src/main/java/com/book/spring_project/SpringProjectApplication.java
+++ spring/spring-project/src/main/java/com/book/spring_project/SpringProjectApplication.java
@@ -11,59 +11,81 @@ import java.io.FileWriter;
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(SpringProjectApplication.class, args);
-
-		String[] beanNames = context.getBeanDefinitionNames();
-
-		for (String bean : beanNames) {
-			System.out.println(bean);
-		}
-
-		Computer computer=context.getBean("computer",Computer.class);
-		Computer computer1=(Computer) context.getBean("computer1");
-		Computer computer2=(Computer) context.getBean("computer2");
-		Computer computer3=(Computer) context.getBean("computer3");
-
-
-		Student student=context.getBean("student",Student.class);
-		Student student1=context.getBean("student1",Student.class);
-		Student student2=context.getBean("student2",Student.class);
-		System.out.println(student.name);
-		System.out.println(student1.name);
-		System.out.println(student2.name);
-
-		Library library=context.getBean(Library.class);
-//		Engine engine=context.getBean(Engine.class);
-//		Engine engine2=context.getBean(Engine.class);
-//		Engine engine3=context.getBean(Engine.class);
-
-		System.out.println(library.name+" "+library.location);
-
-		System.out.println(computer.brand);
-		System.out.println(computer1.brand);
-		System.out.println(computer2.brand);
-		System.out.println(computer3.brand);
-
-		CarService carService=context.getBean(CarService.class);
-
-		carService.startCar();
-		carService.startCar();
-		carService.startCar();
+		 SpringApplication.run(SpringProjectApplication.class, args);
 
 
 
-//		Car car=context.getBean(Car.class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		String[] beanNames = context.getBeanDefinitionNames();
 //
-//		Car car2=context.getBean(Car.class);
-//		Car car3=context.getBean(Car.class);
-//		Car car4=context.getBean(Car.class);
+//		for (String bean : beanNames) {
+//			System.out.println(bean);
+//		}
 //
-////		Person person=context.getBean(Person.class);
+//		Computer computer=context.getBean("computer",Computer.class);
+//		Computer computer1=(Computer) context.getBean("computer1");
+//		Computer computer2=(Computer) context.getBean("computer2");
+//		Computer computer3=(Computer) context.getBean("computer3");
 //
-//		System.out.println(car==car2);
 //
-//		User user= (User) context.getBean("newUserBean");
-//		System.out.println(user.name);
+//		Student student=context.getBean("student",Student.class);
+//		Student student1=context.getBean("student1",Student.class);
+//		Student student2=context.getBean("student2",Student.class);
+//		System.out.println(student.name);
+//		System.out.println(student1.name);
+//		System.out.println(student2.name);
+//
+//		Library library=context.getBean(Library.class);
+////		Engine engine=context.getBean(Engine.class);
+////		Engine engine2=context.getBean(Engine.class);
+////		Engine engine3=context.getBean(Engine.class);
+//
+//		System.out.println(library.name+" "+library.location);
+//
+//		System.out.println(computer.brand);
+//		System.out.println(computer1.brand);
+//		System.out.println(computer2.brand);
+//		System.out.println(computer3.brand);
+//
+//		CarService carService=context.getBean(CarService.class);
+//
+//		carService.startCar();
+//		carService.startCar();
+//		carService.startCar();
+//
+//
+//
+////		Car car=context.getBean(Car.class);
+////
+////		Car car2=context.getBean(Car.class);
+////		Car car3=context.getBean(Car.class);
+////		Car car4=context.getBean(Car.class);
+////
+//////		Person person=context.getBean(Person.class);
+////
+////		System.out.println(car==car2);
+////
+////		User user= (User) context.getBean("newUserBean");
+////		System.out.println(user.name);
 
 
 
--- spring/spring-project/src/main/java/com/book/spring_project/application/Main.java
+++ spring/spring-project/src/main/java/com/book/spring_project/application/Main.java
@@ -1,10 +1,14 @@
 package com.book.spring_project.application;
 
 import com.book.spring_project.SpringProjectApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringProjectApplication.class, args);
             OrdinaryClass ordinaryClass = context.getBean(OrdinaryClass.class);
@@ -16,5 +20,9 @@ public class Main {
 
         MailProperties mailProperties = context.getBean(MailProperties.class);
         System.out.println(mailProperties);
+        log.info("Mail Properties: {}", mailProperties);
+        log.warn("Salam warning");
+        log.error("Salam error");
+        log.debug("Salam error");
     }
 }
